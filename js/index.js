3ff948072c747a128306afc02c8b930689a9f652
DESKTOP-9PJHLMI\USER <2055675594@qq.com>
2022-10-13 19:43:26 +0800

    update

--- js/index.js
+++ js/index.js
@@ -7,9 +7,10 @@ function addApps(apps) {
             <div class=" jesse205-card jesse205-hoverable jesse205-shadow-transition mdui-card mdui-ripple">\
                 <div class="mdui-card-media">\
                     <img src="'+ content.image + '" alt="'+ content.name + '" />\
-                    <div class="mdui-card-media-covered">\
-                        <div class="mdui-card-primary">\
-                            <div class="mdui-card-primary-title">'+ content.name + '</div>\
+                    <div class="jesse205-card-media-covered-white mdui-card-media-covered mdui-card-media-covered-transparent">\
+                        <div class="jesse205-card-primary mdui-card-primary">\
+                            <div class="jesse205-card-primary-title mdui-card-primary-title">'+ content.name + '</div>\
+                            <div class="jesse205-card-primary-subtitle mdui-card-primary-subtitle">'+ content.summary + '</div>\
                         </div>\
                     </div>\
                 </div>\
