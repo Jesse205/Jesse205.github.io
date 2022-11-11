6ee108d85285228e8bf11c21827f0adef7db81ca
Jesse205 <2055675594@qq.com>
2022-11-11 23:05:57 +0800

    更新哈兔box官网，更新PWA为真PWA应用

---
+++ baiduhomelite/serviceWorker.js
@@ -0,0 +1,34 @@
+var cacheName = 'BaiduHomeLite-v1';
+var appShellFiles = [
+    './',
+    'index.html',
+    'logo.png',
+    'favicon.ico',
+    'favicon-16x16.png',
+    'favicon-32x32.png',
+]
+this.addEventListener('install', function (event) {
+    console.log('[Service Worker] Install', event);
+    event.waitUntil(
+        caches.open(cacheName).then(function (cache) {
+            console.log('[Service Worker] Caching all: app shell and content');
+            return cache.addAll(appShellFiles);
+        })
+    );
+});
+
+this.addEventListener('fetch', function (event) {
+    console.log("onFetch", event)
+    event.respondWith(
+        caches.match(event.request).then(function (r) {
+            console.log('[Service Worker] Fetching resource: ' + event.request.url);
+            return r || fetch(event.request).then(function (response) {
+                return caches.open(cacheName).then(function (cache) {
+                    console.log('[Service Worker] Caching new resource: ' + event.request.url);
+                    cache.put(event.request, response.clone());
+                    return response;
+                });
+            });
+        })
+    );
+});
---
+++ deskclocklite/serviceWorker.js
@@ -0,0 +1,33 @@
+var cacheName = 'DeskClockLite-v1';
+var appShellFiles = [
+    './',
+    'index.html',
+    'favicon.ico',
+    'favicon-16x16.png',
+    'favicon-32x32.png',
+]
+this.addEventListener('install', function (event) {
+    console.log('[Service Worker] Install', event);
+    event.waitUntil(
+        caches.open(cacheName).then(function (cache) {
+            console.log('[Service Worker] Caching all: app shell and content');
+            return cache.addAll(appShellFiles);
+        })
+    );
+});
+
+this.addEventListener('fetch', function (event) {
+    console.log("onFetch", event)
+    event.respondWith(
+        caches.match(event.request).then(function (r) {
+            console.log('[Service Worker] Fetching resource: ' + event.request.url);
+            return r || fetch(event.request).then(function (response) {
+                return caches.open(cacheName).then(function (cache) {
+                    console.log('[Service Worker] Caching new resource: ' + event.request.url);
+                    cache.put(event.request, response.clone());
+                    return response;
+                });
+            });
+        })
+    );
+});
