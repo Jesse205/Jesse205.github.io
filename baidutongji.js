e5290a42213ce19c3dd6de1e8754bd6335531b56
Jesse205 <2055675594@qq.com>
2023-09-17 03:43:03 +0800

    添加百度统计

---
+++ baidutongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?1ac0a1e9bbfa196f6c91355c59f01823";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
